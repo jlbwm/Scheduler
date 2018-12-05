d3e21ec09c1d5e6e632ae1fce39261c64bfc6fb7
JMOatey <jackoman100@gmail.com>
2018-12-05 15:01:48 -0600

    Beginning of task view

--- Calender/TaskViewController.swift
+++ Calender/TaskViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class TaskViewController: UIViewController {
 
+    @IBOutlet weak var titleText: UITextField!
+    @IBOutlet weak var locationText: UITextField!
+    @IBOutlet weak var dateText: UITextField!
+    @IBOutlet weak var timeText: UITextField!
+    
+    private var datePicker: UIDatePicker?
+    private var timePicker: UIDatePicker?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Add a task"
-
+        
+        datePicker = UIDatePicker();
+        datePicker?.datePickerMode = .date
+        
+        timePicker = UIDatePicker();
+        timePicker?.datePickerMode = .time
+        
+        dateText.inputView = datePicker
+        datePicker?.addTarget(self, action: #selector(TaskViewController.dateChanged(datePicker:)), for: .valueChanged)
+        
+        timeText.inputView = timePicker
+        timePicker?.addTarget(self, action: #selector(TaskViewController.timeChanged(timePicker:)), for: .valueChanged)
+        
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func timeChanged(timePicker: UIDatePicker){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "h:mm a"
+        timeText.text = dateFormatter.string(from: timePicker.date)
+        view.endEditing(true)
+    }
+    
+    @objc func dateChanged(datePicker: UIDatePicker){
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        dateText.text = dateFormatter.string(from: datePicker.date)
+        view.endEditing(true)
     }
     
 
