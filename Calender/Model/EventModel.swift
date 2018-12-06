072dfc31d0b8ce1d325d020da5e9972bf346c757
Dylan Mouser <codecruncher@mwc-053055.dhcp.missouri.edu>
2018-12-05 21:10:01 -0600

    Updated EventModel to Class

--- Calender/Model/EventModel.swift
+++ Calender/Model/EventModel.swift
@@ -8,70 +8,72 @@
 
 import Foundation
 
-struct User {
-    var weekdayStartTime: Date
-    var weekdayEndTime: Date
-    var weekendStartTime: Date
-    var weekendEndTime: Date
-    var isMorningPerson: Bool
-    var advanceDate: Int
-    var isSplit: Bool
-    var isDifficult: Bool
-}
-
-struct ReoccuringEvent {
-    var title: String
-    var location: String
-    var startDate: Date
-    var endDate: Date
-    var startTime: Date
-    var endTime: Date
-    var category: Category
-    var week: [Week]
-}
-
-struct StandAloneEvent{
-    var title: String
-    var location: String
-    var startDate: Date
-    var StartTime: Date
-    var EndTime: Date
-    var category: Category
-}
-
-struct Tasks{
-    var title: String
-    var location: String
-    var deadline: Date
-    var isSplittable: Bool
-    var category: Category
-    var Priority: Int
-    var Difficult: Int
-    //var ExpectedAllocatedTime: TimeEnum
-}
-
-enum Category {
-    case school
-    case work
-    case social
-    case fitness
-    case other
-}
-
-enum Frequence {
-    case Daily
-    case Weekly
-    case Monthly
-    case None
+class EventModel {
+    struct User {
+        var weekdayStartTime: Date
+        var weekdayEndTime: Date
+        var weekendStartTime: Date
+        var weekendEndTime: Date
+        var isMorningPerson: Bool
+        var advanceDate: Int
+        var isSplit: Bool
+        var isDifficult: Bool
+    }
     
-}
-
-enum Week: String {
-    case sunday
-    case monday
-    case tuesday
-    case wednesday
-    case thursday
-    case friday
-    case saturdays
+    struct ReoccuringEvent {
+        var title: String
+        var location: String
+        var startDate: Date
+        var endDate: Date
+        var startTime: Date
+        var endTime: Date
+        var category: Category
+        var week: [Week]
+    }
+    
+    struct StandAloneEvent{
+        var title: String
+        var location: String
+        var startDate: Date
+        var StartTime: Date
+        var EndTime: Date
+        var category: Category
+    }
+    
+    struct Tasks{
+        var title: String
+        var location: String
+        var deadline: Date
+        var isSplittable: Bool
+        var category: Category
+        var Priority: Int
+        var Difficult: Int
+        //var ExpectedAllocatedTime: TimeEnum
+    }
+    
+    enum Category {
+        case school
+        case work
+        case social
+        case fitness
+        case other
+    }
+    
+    enum Frequence {
+        case Daily
+        case Weekly
+        case Monthly
+        case None
+        
+    }
+    
+    enum Week: String {
+        case sunday
+        case monday
+        case tuesday
+        case wednesday
+        case thursday
+        case friday
+        case saturdays
+    }
 }
